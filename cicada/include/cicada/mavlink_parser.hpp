851abb310a8b81aa2932600ec6eb1193d8848ab3
Spencer Muller <spencer.muller@edu.sait.ca>
2026-05-20 21:38:04 -0600

    Started to implement mavlink_parser.hpp starting with UDP socket opener class implementation. Updated gitignore to hide more build artifacts

--- cicada/include/cicada/mavlink_parser.hpp
+++ cicada/include/cicada/mavlink_parser.hpp
@@ -1,1 +1,43 @@
 #pragma once
+#include <ardupilotmega/mavlink.h>
+#include <winsock2.h> // windows specific networking libs
+#include <ws2tcpip.h>
+#include <stdexcept>
+#pragma comment(lib, "ws2_32.lib")
+
+
+
+class UDPOPEN  // UDP socket opener class
+{
+    private:
+    SOCKET sock;
+    sockaddr_in localAddr;
+    uint8_t buffer[512];
+    mavlink_message_t msg;
+    mavlink_status_t status;
+    WSADATA wsaData;
+public:
+    UDPOPEN(){   
+        WSAStartup(  // init winsock
+           MAKEWORD(2, 2), &wsaData     
+        );
+
+         int result = WSAStartup(MAKEWORD(2,2), &wsaData); // check if process is up
+        if(result != 0){
+            throw std::runtime_error("WSAStartup Failed");
+        }
+
+        sock = ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // define the win socket for udp
+        if(sock == INVALID_SOCKET){
+            throw std::runtime_error("socket failed");
+        }
+    }
+    
+
+    void receive() const{};
+
+    ~UDPOPEN(){
+
+    }
+    
+};
