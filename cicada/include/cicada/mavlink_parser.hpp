11c4d713a331f22ce951ae6a9fb9cb6d4f2d6f3a
Spencer Muller <spencer.muller@edu.sait.ca>
2026-05-20 23:48:45 -0600

    added socket bind, and started the parser implementation

--- cicada/include/cicada/mavlink_parser.hpp
+++ cicada/include/cicada/mavlink_parser.hpp
@@ -5,40 +5,53 @@
 #include <stdexcept>
 #pragma comment(lib, "ws2_32.lib") // TODO migrate to fit with gcc toolchain
 
-
-
-class UdpSocket  // UDP socket opener class
+class UdpSocket // UDP socket opener class
 {
-    private:
+private:
     SOCKET sock;
     sockaddr_in localAddr;
-    uint8_t buffer[2048]; // update to fit mavsdk frame output
-    mavlink_message_t msg;
-    mavlink_status_t status;
     WSADATA wsaData;
+
 public:
-    UdpSocket(){   
-    
-        int result = WSAStartup(MAKEWORD(2,2), &wsaData); // check if process is up
-        if(result != 0){
+    UdpSocket()
+    {
+
+        int result = WSAStartup(MAKEWORD(2, 2), &wsaData); // check if process is up
+        if (result != 0)
+        {
             throw std::runtime_error("WSAStartup Failed");
         }
 
         sock = ::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP); // define the win socket for udp
-        if(sock == INVALID_SOCKET){
+        if (sock == INVALID_SOCKET)
+        {
             throw std::runtime_error("socket failed");
         }
+        localAddr.sin_family = AF_INET;
+        localAddr.sin_port = htons(14550);      // mavsdk's default port
+        localAddr.sin_addr.s_addr = INADDR_ANY; // allows packets from any network interface on this ip
+        if (bind(sock, (sockaddr *)&localAddr, sizeof(localAddr)) == SOCKET_ERROR)
+        {
+            throw std::runtime_error("Bind failed");
+        };
     }
-    
 
-    void receive() {
-        
-    };
-     // destruct socket 
-    ~UdpSocket(){
+    int receive(uint8_t *buffer, int bufferSize) {}; // recieves raw bytes
+    // TODO: receive implementation
+
+    // destruct socket
+    ~UdpSocket()
+    {
         closesocket(sock);
         WSACleanup();
-       
     }
-    
+};
+
+class MavLinkParser
+{
+    mavlink_message_t msg;
+    mavlink_status_t status;
+    uint8_t buffer[2048]; // update to fit mavsdk frame output
+public:
+    // TODO: Parser constructor and parse method
 };
